23d91251cc17f7363bf979dcc40c0c18c16e8956
unknown <kimank@.(none)>
2011-10-23 21:28:03 +0400

    Committer: nikloaj <kimank@.(none)> 	new file:   analyzer/analyzer.iml 	new file:   analyzer/src/ru/brandanalyst/analyzer/Analyzer.java 	modified:   BrandAnalytics.ipr 	modified:   BrandAnalytics.iws 	modified:   analyzer/analyzer.iml 	modified:   analyzer/src/ru/brandanalyst/analyzer/Analyzer.java

---
+++ analyzer/src/ru/brandanalyst/analyzer/Analyzer.java
@@ -0,0 +1,28 @@
+package ru.brandanalyst.analyzer;
+
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.jdbc.core.simple.SimpleJdbcTemplate;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Nikolaj Karpov
+ * Date: 23.10.11
+ * Time: 21:10
+ */
+public class Analyzer implements InitializingBean{
+    SimpleJdbcTemplate dirty;
+    SimpleJdbcTemplate pure;
+    void setDirty(){
+
+    }
+    void setPure(){
+
+    }
+    public void pushDirtyToPure(){
+
+    }
+    public void afterPropertiesSet(){
+
+        void AnalyzerTwitter();
+    }
+}
