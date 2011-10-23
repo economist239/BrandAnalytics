4946165632b924213a178ecd8a1acf27bf14ab60
unknown <kimank@.(none)>
2011-10-23 21:30:32 +0400

    modified:   analyzer/analyzer.iml 	modified:   analyzer/src/ru/brandanalyst/analyzer/Analyzer.java

--- analyzer/src/ru/brandanalyst/analyzer/Analyzer.java
+++ analyzer/src/ru/brandanalyst/analyzer/Analyzer.java
@@ -2,6 +2,12 @@ package ru.brandanalyst.analyzer;
 
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.jdbc.core.simple.SimpleJdbcTemplate;
+import ru.brandanalyst.core.db.provider.ArticleProvider;
+import ru.brandanalyst.core.db.provider.BrandProvider;
+import ru.brandanalyst.core.model.Article;
+import ru.brandanalyst.core.model.Brand;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -12,17 +18,25 @@ import org.springframework.jdbc.core.simple.SimpleJdbcTemplate;
 public class Analyzer implements InitializingBean{
     SimpleJdbcTemplate dirty;
     SimpleJdbcTemplate pure;
-    void setDirty(){
-
+    void setDirty(SimpleJdbcTemplate dirty){
+        this.dirty = dirty;
     }
-    void setPure(){
-
+    void setPure(SimpleJdbcTemplate pure){
+        this.pure  = pure;
     }
-    public void pushDirtyToPure(){
-
+    public void pushBrandsDirtyToPure(){
+        BrandProvider from = new BrandProvider(dirty);
+        BrandProvider to   = new BrandProvider(pure);
+        to.writeListOfBrandsToDataStore(from.getAllBrands());
+    }
+    public void pushArticlesDirtyToPure(){
+        ArticleProvider from = new ArticleProvider(dirty);
+        ArticleProvider to   = new ArticleProvider(pure);
+        to.writeListOfArticlesToDataStore(from.getAllArticles());
     }
     public void afterPropertiesSet(){
-
+        pushArticlesDirtyToPure();
+        pushBrandsDirtyToPure();
         void AnalyzerTwitter();
     }
 }
