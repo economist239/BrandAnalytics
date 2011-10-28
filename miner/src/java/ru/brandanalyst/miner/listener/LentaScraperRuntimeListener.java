5706d44674b9353dab4a73a5c6bd658365e5eb33
olegpan <oleg.panjkov@yandex.ru>
2011-10-29 00:24:04 +0400

    Lenta grabber added

---
+++ miner/src/java/ru/brandanalyst/miner/listener/LentaScraperRuntimeListener.java
@@ -0,0 +1,11 @@
+package ru.brandanalyst.miner.listener;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Олег_П
+ * Date: 29.10.11
+ * Time: 0:21
+ * To change this template use File | Settings | File Templates.
+ */
+public class LentaScraperRuntimeListener {
+}
---
+++ miner/src/java/ru/brandanalyst/miner/util/LentaDataTransformator.java
@@ -0,0 +1,11 @@
+package ru.brandanalyst.miner.util;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Олег_П
+ * Date: 29.10.11
+ * Time: 0:20
+ * To change this template use File | Settings | File Templates.
+ */
+public class LentaDataTransformator {
+}
