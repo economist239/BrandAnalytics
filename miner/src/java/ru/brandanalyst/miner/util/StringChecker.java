41cb0b45853033d868913bce0e7aa1adec2575ec
batya <batya239@gmail.com>
2011-11-10 23:39:25 +0400

    string checker fixed

--- miner/src/java/ru/brandanalyst/miner/util/StringChecker.java
+++ miner/src/java/ru/brandanalyst/miner/util/StringChecker.java
@@ -21,17 +21,12 @@ public class StringChecker {
     }
 
     public static boolean hasTerm(BrandDictionaryItem brandItem, String title) { //O(l^2 )
+        title = title.toLowerCase();
 //        for (String item : brandItem.getItems())
 //         if (title.toLowerCase().contains(item.toLowerCase())) return true;
 //        return false;
         for (int i = 0; i < title.length(); i++) {
             for (int j = i + 1; j < title.length(); j++) {
-                if (brandItem.getItems().contains(title.substring(i, j).toUpperCase())) {
-                    return true;
-                }
-                if (brandItem.getItems().contains(title.substring(i, j).toLowerCase())) {
-                    return true;
-                }
                 if (brandItem.getItems().contains(title.substring(i, j))) {
                     return true;
                 }
