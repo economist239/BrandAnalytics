d0f0515973d4b1994cf3e93ef699c5c65c17ef60
Dima <dima@Dimas-MacBook-Pro.local>
2012-01-23 20:03:51 +0400

    workable server building

--- frontend/src/scripts/server.sh
+++ frontend/src/scripts/server.sh
@@ -11,6 +11,6 @@ CP2=lib/*.jar
         -Djavax.xml.transform.TransformerFactory=net.sf.saxon.TransformerFactoryImpl \
         -showversion -server -Xverify:none \
         -Xmx512m -Xms128m -XX:+UseConcMarkSweepGC -XX:+HeapDumpOnOutOfMemoryError \
-        net.sf.xfresh.util.Starter fontend-beans.xml &
+        net.sf.xfresh.util.Starter frontend-beans.xml &
 
     echo "brand-analytics started..."
