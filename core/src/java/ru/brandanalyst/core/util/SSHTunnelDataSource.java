690515451e93f7dadb3a52d73abd9c508eec0cd8
Obus <sensanek@gmail.com>
2012-01-23 16:05:10 +0300

    all connection parameters moved to properties file

--- core/src/java/ru/brandanalyst/core/util/SSHTunnelDataSource.java
+++ core/src/java/ru/brandanalyst/core/util/SSHTunnelDataSource.java
@@ -3,6 +3,7 @@ package ru.brandanalyst.core.util;
 import com.jcraft.jsch.JSch;
 import com.jcraft.jsch.Session;
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.InitializingBean;
 
 import javax.sql.DataSource;
 import java.io.PrintWriter;
@@ -17,20 +18,49 @@ import java.sql.SQLFeatureNotSupportedException;
  * Date: 22.01.12
  * Time: 21:35
  */
-public class SSHTunnelDataSource implements DataSource {
+public class SSHTunnelDataSource implements DataSource , InitializingBean {
     private static final Logger log = Logger.getLogger(SSHTunnelDataSource.class);
-    protected static JSch jsch;
-    protected static Session session;
-    protected static String host="ssh.ba.nichost.ru";
-    protected static String user="ba";
-    protected static String pass="jmmvtsgh";
-    protected static int port=22;
-    protected static int tunnelLocalPort=3336;
-    protected static String tunnelRemoteHost="ba.mysql";
-    protected static int tunnelRemotePort=3306;
-    protected String username = "ba_dev";
-    protected String password = "ieue54lu";
+
+    protected JSch jsch;
+    protected Session session;
+    protected String host;
+    protected String user;
+    protected String pass;
+    protected int port;
+    protected int tunnelLocalPort;
+    protected String tunnelRemoteHost;
+    protected int tunnelRemotePort;
+    protected String username;
+    protected String password;
     protected String url;
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public void setTunnelLocalPort(int tunnelLocalPort) {
+        this.tunnelLocalPort = tunnelLocalPort;
+    }
+
+    public void setTunnelRemoteHost(String tunnelRemoteHost) {
+        this.tunnelRemoteHost = tunnelRemoteHost;
+    }
+
+    public void setTunnelRemotePort(int tunnelRemotePort) {
+        this.tunnelRemotePort = tunnelRemotePort;
+    }
 
     public void close(){
         session.disconnect();
@@ -84,32 +114,31 @@ public class SSHTunnelDataSource implements DataSource {
             log.error("Ошибка при установке таймаута", e);
         }
     } 
-         // Sets the maximum time in seconds that this data source will wait while attempting to connect to a database.
+
     public void	setLogWriter(PrintWriter out){
         
     } 
-         // Sets the log writer for thi
 
-
-    static{
-
+    @Override
+    public void afterPropertiesSet(){
         jsch = new JSch();
-        try{
-            session=jsch.getSession(user, host, port);
-            session.setPassword(pass);
-            
-            java.util.Properties config = new java.util.Properties();
-            config.put("StrictHostKeyChecking", "no");
-            session.setConfig(config);
+        if(null==session || !session.isConnected()){
+            try{
+                session=jsch.getSession(user, host, port);
+                session.setPassword(pass);
 
-            log.info("Открытие ssh соединения");
-            session.connect();
-            log.info("Устанавливаем туннель");
-            session.setPortForwardingL(tunnelLocalPort, tunnelRemoteHost, tunnelRemotePort);
-        }
-        catch(Exception e){
-            log.error("Ошибка при построении соединения", e);
+                java.util.Properties config = new java.util.Properties();
+                config.put("StrictHostKeyChecking", "no");
+                session.setConfig(config);
+
+                log.info("Открытие ssh соединения");
+                session.connect();
+                log.info("Устанавливаем туннель");
+                session.setPortForwardingL(tunnelLocalPort, tunnelRemoteHost, tunnelRemotePort);
+            }
+            catch(Exception e){
+                log.error("Ошибка при построении соединения", e);
+            }
         }
     }
-
 }
