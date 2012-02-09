3cd27f4602d0e10cd1703683cbc82eaa54e3a2a0
batya239 <batya239@gmail.com>
2012-02-09 23:55:16 +0400

    timer task

---
+++ tasker/src/java/ru/brandanalyst/common/scheduler/AbstractDelayedTimerTask.java
@@ -0,0 +1,31 @@
+package ru.brandanalyst.common.scheduler;
+
+import org.springframework.scheduling.timer.ScheduledTimerTask;
+
+import java.util.TimerTask;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: dima
+ * Date: 2/9/12
+ * Time: 10:14 PM
+ *
+ * requires to set only period
+ */
+public abstract class AbstractDelayedTimerTask extends ScheduledTimerTask {
+    private static final long DEFAULT_DELAY = 10000;
+
+    protected AbstractDelayedTimerTask() {
+        super();
+        setDelay(DEFAULT_DELAY);
+        setRunnable(new Runnable() {
+            @Override
+            public void run() {
+                runTask();
+            }
+        });
+ 
+    }
+    
+    public abstract void runTask();
+}
