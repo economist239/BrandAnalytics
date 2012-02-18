95b777d7d5ee45083bab5fdd6813089e081cf722
Dmitry Batkovich <daddy-bear@yandex-team.ru>
2012-02-18 16:46:12 +0400

    kv extending  provider

--- core/src/java/ru/brandanalyst/core/db/provider/interfaces/ArticleProvider.java
+++ core/src/java/ru/brandanalyst/core/db/provider/interfaces/ArticleProvider.java
@@ -1,46 +1,48 @@
 package ru.brandanalyst.core.db.provider.interfaces;
 
+import ru.brandanalyst.core.db.provider.EntityVisitor;
 import ru.brandanalyst.core.db.provider.mysql.MappersHolder;
 import ru.brandanalyst.core.model.Article;
 import ru.brandanalyst.core.model.ArticleForWeb;
 
 import java.util.List;
 
-public interface ArticleProvider {
-    int MAX_ARTICLE_LENGHT = 30000;
+public abstract class ArticleProvider {
+    protected final static int MAX_ARTICLE_LENGHT = 30000;
 
-    void writeArticleToDataStore(Article article);
+    public abstract void writeArticleToDataStore(Article article);
 
-    void writeListOfArticlesToDataStore(List<Article> articles);
+    public abstract void writeListOfArticlesToDataStore(List<Article> articles);
 
-    Article getArticleBySourceId(long sourceId);
+    public abstract Article getArticleBySourceId(long sourceId);
 
-    List<Article> getAllArticlesBySourceId(long sourceId);
+    public abstract List<Article> getAllArticlesBySourceId(long sourceId);
 
-    Article getArticleById(long articleId);
+    public abstract Article getArticleById(long articleId);
 
-    ArticleForWeb getArticleForWebById(long articleId);
+    public abstract ArticleForWeb getArticleForWebById(long articleId);
 
-    List<Article> getAllArticlesByBrand(long brandId);
+    public abstract List<Article> getAllArticlesByBrand(long brandId);
 
-    List<Article> getAllOfficialArticlesByBrand(long brandId);
+    public abstract List<Article> getAllOfficialArticlesByBrand(long brandId);
 
-    List<Article> getAllArticles();
+    public abstract List<Article> getAllArticles();
 
-    List<Article> getArticlesWithCondition(String whereClause);
+    public abstract List<Article> getArticlesWithCondition(String whereClause);
 
+    public abstract void visitArticles(EntityVisitor<Article> visitor);
     /*
      * возращает указанное количество самых свежих новостей по данном бренду
      */
-    List<Article> getTopArticles(long brandId, int topSize);
+    public abstract List<Article> getTopArticles(long brandId, int topSize);
 
-    List<Article> getAllArticlesByBrandAndSource(long brandId, long sourceId);
+    public abstract List<Article> getAllArticlesByBrandAndSource(long brandId, long sourceId);
 
     /*
     *
     * only for dirty db!!
     */
-    void setAnalyzed(final List<Long> ids);
+    public abstract void setAnalyzed(final List<Long> ids);
 
-    List<Article> getOnlyNotAnalyzedArticles();
+    public abstract List<Article> getOnlyNotAnalyzedArticles();
 }
