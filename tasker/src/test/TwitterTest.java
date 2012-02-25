b92ddb2c0200355594edd12ef96021f84b3c3d35
vans239 <vans239@gmail.com>
2012-02-25 15:27:19 +0400

    twitter test rewrote

--- tasker/src/test/TwitterTest.java
+++ tasker/src/test/TwitterTest.java
@@ -1,6 +1,7 @@
 import com.thoughtworks.xstream.XStream;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import ru.brandanalyst.AbstractTest;
 import ru.brandanalyst.miner.twitter.ITwitter;
 import ru.brandanalyst.miner.twitter.ITwitterException;
 import ru.brandanalyst.miner.twitter.impl.SimpleTwitter;
@@ -11,13 +12,23 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import static ru.brandanalyst.miner.twitter.ITwitter.TwitterResultItem;
+
+
 /**
  * @author Vanslov Evgeny (evans@yandex-team.ru)
  */
-public class TwitterTest {
+public class TwitterTest extends AbstractTest{
     private static final Log log = LogFactory.getLog(TwitterTest.class);
 
-    public static void main(String[] args) throws ITwitterException, IOException {
+    protected ITwitter twitter;
+
+    private String configLocations[] = {"test-config.xml"};
+    @Override
+    protected String[] getConfigLocations() {
+        return configLocations;
+    }
+    public void testTwitter() throws ITwitterException, IOException {
         String companies[] = {
                 "google",
                 /*"Сбербанк",
@@ -43,10 +54,10 @@ public class TwitterTest {
         };
 
         log.info("Mining topsy started");
-        List<ITwitter.TwitterResultItem> companiesTweets = new ArrayList<ITwitter.TwitterResultItem>();
+        List<TwitterResultItem> companiesTweets = new ArrayList<TwitterResultItem>();
         for (String company : companies) {
             log.info("Company now: " + company);
-            ITwitter.TwitterResultItem topsyItem = new SimpleTwitter().getPopularTweets(company);
+            TwitterResultItem topsyItem = twitter.getPopularTweets(company);
             companiesTweets.add(topsyItem);
             log.info("Company done; " + company + " Found: " + topsyItem.tweets.size());
         }
