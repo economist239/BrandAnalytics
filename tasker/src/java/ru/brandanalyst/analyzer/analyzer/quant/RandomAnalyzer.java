2d575399448e56ccacf7c298e4650997c6b7af50
Alexandra Mikhaylova <mikhaylova.alexandra.a@gmail.com>
2012-03-11 03:34:24 +0400

    Random classifier added + classifiers structure refactored + Weka SVM classifiers folder moved to module tasker

--- tasker/src/java/ru/brandanalyst/analyzer/analyzer/quant/RandomAnalyzer.java
+++ tasker/src/java/ru/brandanalyst/analyzer/analyzer/quant/RandomAnalyzer.java
@@ -1,32 +1,31 @@
 package ru.brandanalyst.analyzer.analyzer.quant;
 
-import ru.brandanalyst.core.db.provider.ProvidersHandler;
-import ru.brandanalyst.core.db.provider.interfaces.GraphProvider;
+import java.util.Random;
 import ru.brandanalyst.core.model.Article;
 
 /**
  * Alexandra Mikhaylova mikhaylova@yandex-team.ru
  */
-public class RandomAnalyzer implements AbstractAnalyzer {
-    private GraphProvider graphProvider;
-
-    @Override
-    public void init(ProvidersHandler pureProvidersHandler) {
-        this.graphProvider = pureProvidersHandler.getGraphProvider();
-    }
+public class RandomAnalyzer extends AbstractSentimentAnalyzer {
+    private Random randomGenerator;
 
     @Override
     public void onStart() {
-        //To change body of implemented methods use File | Settings | File Templates.
+        super.onStart();
+        this.randomGenerator = new Random();
     }
 
     @Override
     public void analyze(Article article) {
-        //To change body of implemented methods use File | Settings | File Templates.
-    }
-
-    @Override
-    public void flush() {
-        //To change body of implemented methods use File | Settings | File Templates.
+        final double clsRandom = this.randomGenerator.nextInt(3) - 1;
+        if (clsRandom == -1.0) {
+            addPointToGraph(article, article.getBrandId(), this.graphDepotNegative);
+        }
+        if (clsRandom == 0.0) {
+            addPointToGraph(article, article.getBrandId(), this.graphDepotNeutral);
+        }
+        if (clsRandom == 1.0) {
+            addPointToGraph(article, article.getBrandId(), this.graphDepotPositive);
+        }
     }
 }
