f50bec75bdec22966536e8b507f82c6862a2f0a1
batya <batya239@gmail.com>
2012-05-05 07:53:37 +0400

    cx

--- rest-brand-analytics/src/scala/ru/brandanalyst/rest/MainRouter.scala
+++ rest-brand-analytics/src/scala/ru/brandanalyst/rest/MainRouter.scala
@@ -1,8 +1,15 @@
 package ru.brandanalyst.rest
 
+import ru.circumflex.web._
 /**
  * @author daddy-bear
  *         Date: 05.05.12 - 7:44
  */
+
+class MainRouter extends Router {
+  get("/") = {
+    ftl("/index.html.ftl")
+  }
+}
 
 
